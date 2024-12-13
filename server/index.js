4e6d81610d33c35120a8914b51e68c98a0957863
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2024-12-14 02:11:05 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+});
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Welcome to the backend!' });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
